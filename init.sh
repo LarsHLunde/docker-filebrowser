e2385ef72a70fbdc318be3d4d13da40fee945514
Lars Hudson Lunde <laffen47@yahoo.no>
2023-11-12 21:12:29 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,9 @@
 FILE=/script/filebrowser.json
 if test -f "$FILE"; then
     mv /script/filebrowser.json /config/.
+    echo "Docker Filebrowser has been installed"
+    echo "Start the docker the normal way and it will start normally"
+    exit 0
 fi
 
 /usr/local/bin/filebrowser -c /config/filebrowser.json
